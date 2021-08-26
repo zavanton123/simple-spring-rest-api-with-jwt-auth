774d303979ee3ea0091c9243f534b5e9c7c7f626
zavanton123 <zavanton123@gmail.com>
2021-08-26 14:45:54 +0300

    init

---
+++ src/main/kotlin/com/evolunta/api/EvoluntaApiApplication.kt
@@ -0,0 +1,11 @@
+package com.evolunta.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EvoluntaApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<EvoluntaApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/evolunta/api/EvoluntaApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.evolunta.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EvoluntaApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
